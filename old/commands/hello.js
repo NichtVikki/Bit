a947441359169fea4d3b37768581e6b421417c30
NisamVikki <viktorblazaric2@gmail.com>
2022-11-28 19:36:14 +0100

    Removed the old folder

--- old/commands/hello.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    aliases: ['hi'],
-    category: "Help",
-    callback: ({ message }) => {
-        message.lineReply("hi i guess?")
-    }
-}
--- old/commands/info.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    aliases: ['about-me'],
-    category: "Help",
-    callback: ({ message }) => {
-        message.lineReply("a normal bot, lol idk what else to say")
-    }
-}
